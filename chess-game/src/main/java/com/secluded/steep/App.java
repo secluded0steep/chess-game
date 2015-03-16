60dbf2b18ea932fdb2f82c66d574080b6ace6ab8
secluded0steep <pete@gmail.com>
2015-03-16 23:09:22 +0800

    Test project

---
+++ chess-game/src/main/java/com/secluded/steep/App.java
@@ -0,0 +1,13 @@
+package com.secluded.steep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
